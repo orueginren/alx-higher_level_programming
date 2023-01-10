d34a5e5b4d19c5d0576fa10073bd5c95cbc67c59
orueginren <ijawboi360@gmail.com>
2023-01-10 05:02:31 +0000

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+
     from add_0 import add
 
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+    a = int(a)
+    b = int(b)
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
